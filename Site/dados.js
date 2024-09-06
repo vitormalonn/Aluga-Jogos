bbee6af2c438aba3eb6a8dfc32a670b2dc5a07ba
vitor <78054135+vitormalonn@users.noreply.github.com>
2024-09-05 22:50:07 -0300

    Aluga Jogos 0.1

---
+++ Site/dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo : "Life is Feudal: Forest Village",
+        descricao : "Os jogos estão disponiveis temporariamente, com um temporizador de tempo. Mais detalhes em De acordo com os Artigos 565 a 578.",
+        genero : "Indie, simulação, estratégia e gestão"
+        link : "https://www.cpt.com.br/codigo-civil/tipos-de-contrato-locacao-de-coisas"
+    },
+    {
+        titulo : "GTA V",
+        descricao : "Os jogos estão disponiveis temporariamente, com um temporizador de tempo. Mais detalhes em De acordo com os Artigos 565 a 578.",
+        genero : "Ação e aventura"
+        link : "https://www.cpt.com.br/codigo-civil/tipos-de-contrato-locacao-de-coisas"
+    },
+    {
+        titulo : "RimWorld",
+        descricao : "Os jogos estão disponiveis temporariamente, com um temporizador de tempo. Mais detalhes em De acordo com os Artigos 565 a 578.",
+        genero : "Indie, Simulação, estratégia e gestão"
+        link : "https://www.cpt.com.br/codigo-civil/tipos-de-contrato-locacao-de-coisas"
+    },
+    {
+        titulo : "Total Conflit",
+        descricao : "Os jogos estão disponiveis temporariamente, com um temporizador de tempo. Mais detalhes em De acordo com os Artigos 565 a 578.",
+        genero : "Ação, Simulação, Estratégia e getão"
+        link : "https://www.cpt.com.br/codigo-civil/tipos-de-contrato-locacao-de-coisas"
+    },
+    {
+        titulo : "7Days To Die",
+        descricao : "Os jogos estão disponiveis temporariamente, com um temporizador de tempo. Mais detalhes em De acordo com os Artigos 565 a 578.",
+        genero : "Ação, Aventura, Indie, RPG, Simulação, Estratégia e gestão"
+        link : "https://www.cpt.com.br/codigo-civil/tipos-de-contrato-locacao-de-coisas"
+    },
+]
